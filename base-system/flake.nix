0dc3f78917c660fdd80e01edc0b91ae9d87a74b8
twoneis <sanjay29@sapura.de>
2023-10-30 08:05:26 +0100

    initial commit

---
+++ base-system/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Nix flake for basic configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inptus@{nixpkgs, ...}:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+
+      config = {
+        allowUnfree = true;
+      };
+    };
+  in
+  {
+    nixosConfigurations = {
+      nixdesktop = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit system; };
+
+        modules = [ 
+          ./configuration.nix 
+        ];
+      };
+    };
+  };
+}
