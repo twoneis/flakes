6034d0eddfad85be2df8568e6e6a9a1cb88d2420
twoneis <sanjay29@sapura.de>
2023-10-31 21:43:53 +0100

    added flake for markdown

--- markdown/flake.nix
+++ markdown/flake.nix
@@ -1,11 +1,23 @@
 {
-  description = "A very basic flake";
+  description = "A template flake for development with gcc";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShell.x86_64-linux = pkgs.mkShell rec {
+        name = "markdown";
+        packages = with pkgs; [
+          marksman
+        ];
+        shellHook = ''export PS1="[\u@gcc-devel:\W]\$ "'';
+      };
+    };
 }
