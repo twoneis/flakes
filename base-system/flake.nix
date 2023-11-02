56cd99d8bfc10333d63ae4392a9b0a354e6d1129
twoneis <sanjay29@sapura.de>
2023-11-02 09:44:12 +0100

    started home-manager config

--- base-system/flake.nix
+++ base-system/flake.nix
@@ -1,11 +1,18 @@
 {
-  description = "Nix flake for basic configuration";
+  description = "Nix flake for basic configuration, for surface and desktop";
+
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{nixpkgs, ...}:
+
+  outputs = inputs@{nixpkgs, home-manager, ...}:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -22,6 +29,12 @@
 
       modules = [ 
         ./desktop-config.nix 
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.twoneis = import ./home.nix
+        }
       ];
     };
     nixConfigurations.nixsurface = nixpkgs.lib.nixosSystem {
@@ -29,6 +42,12 @@
 
       modules = [
         ./surface-config.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.twoneis = import ./home.nix
+        }
       ];
     };
   };
---
+++ base-system/home.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }
+
+{
+  # Set username and home directory
+  home.username = "twoneis";
+  home.homeDirectory = "/home/twoneis";
+
+  # Enable home manager (with self config)
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "twoneis";
+    userEmail = "sanjay29@sapura.de";
+  }
+}
