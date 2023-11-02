fb39adcab6dfa652c81e70c3ba676e3afa0c75d9
twoneis <sanjay29@sapura.de>
2023-11-02 05:38:50 +0100

    changed kernel version

--- base-system/surface-config.nix
+++ base-system/surface-config.nix
@@ -20,6 +20,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Select kernel version
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   networking.hostName = "nixsurface"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
