805650aafdbe5692e4d0dcb1f48b3a86b1a71b75
twoneis <sanjay29@sapura.de>
2023-11-03 07:37:21 +0100

    added bash aliases to config

--- base-system/home.nix
+++ base-system/home.nix
@@ -5,14 +5,17 @@
   home.username = "twoneis";
   home.homeDirectory = "/home/twoneis";
 
-  home.shellAliases = {
-    "up" = "nix flake update /home/twoneis/.flakes/base-system &&
-            sudo nixos-rebuild switch --flake /home/twoneis/.flakes/base-system";
-  };
-
   programs = {
     home-manager = {
       enable = true;
+    };
+
+    bash = {
+      shellAliases = {
+        up = "nix flake update /home/twoneis/.flakes/base-system &&
+              sudo nixos-rebuild switch --flake /home/twoneis/.flakes/base-system";
+        ll = "ls -al";
+      };
     };
 
     git = {
