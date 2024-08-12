81c473bca55055e36b37dd41d919196d21c68ea6
twoneis <sanjay29@sapura.de>
2024-08-12 21:36:35 +0200

    typst

--- flake.nix
+++ flake.nix
@@ -16,5 +16,15 @@
       path = ./rust;
       description = "A simple flake with an overlay for rust";
     };
+
+    c = {
+      path = ./c;
+      description = "Flake for c or cpp development";
+    };
+
+    typst = {
+      path = ./typst;
+      description = "A flake for automatically compiling typst files";
+    };
   };
 }
